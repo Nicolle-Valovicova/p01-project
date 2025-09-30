2addf362bd1f4e6456255a2bc19b64419264f0ea
Nicolle Valovicova <302245247@student.rocmondriaan.nl>
2025-09-30 20:17:09 +0200

    achtergrondkleuren geven aan goed/fout antwoord

--- script-nicolle.js
+++ script-nicolle.js
@@ -1,7 +1,9 @@
 let questionShow = document.getElementById("questionShow");
-let answersShow = document.querySelectorAll("answersShow");
+let answersShow = document.querySelectorAll(".answersShow");
 let prevBtn = document.getElementById("backbtn");
 let nextBtn = document.getElementById("nextBtn");
+let powerUp = document.getElementById("powerUp");
+
 let ourQuestions = "questions.json";
 // shuffle function for ramdom ex. answers
 const shuffle = (array) => {
@@ -19,12 +21,7 @@ fetch(ourQuestions)
   .then((data) => {
     console.log(data.nicolle);
     let currentIndex = 0;
-
-    let randomAnswers = shuffle(data.nicolle[i].options);
-    console.log(randomAnswers);
-    document.querySelectorAll(".answersShow").forEach((li, i) => {
-      li.innerHTML = randomAnswers[i];
-    }); //shuffled options for quiz
+    //shuffled options for quiz
 
     showQuestion(currentIndex); //display the different questions by using the next button
     showOptions(currentIndex);
@@ -33,8 +30,9 @@ fetch(ourQuestions)
       if (currentIndex < data.nicolle.length) {
         showQuestion(currentIndex);
         showOptions(currentIndex);
+        resetBgColor();
       } else {
-        console.log("quiz finished");
+        console.log("and nothing shows");
       }
       console.log("btn clicked");
     });
@@ -43,19 +41,44 @@ fetch(ourQuestions)
         currentIndex--;
 
         showQuestion(currentIndex);
-        console.log("tz");
+        showOptions(currentIndex);
+        resetBgColor();
       } else {
-        console.log("ehfehfugzgfwuzguwzg");
+        console.log("nothing works");
       }
     });
     function showQuestion(currentIndex) {
       questionShow.innerHTML = data.nicolle[currentIndex].question;
     } //display the questions in your html
     function showOptions(currentIndex) {
-      document.querySelectorAll(".answersShow").forEach((li, i) => {
-        li.innerHTML = randomAnswers[currentIndex];
+      const opts = data.nicolle[currentIndex].options;
+      const shuffled = shuffle([...opts]);
+      answersShow.forEach((li, i) => { // show the options fron json array in the html shuffled
+        li.innerHTML = shuffled[i];
       });
+      console.log("options:", opts);
+      console.log("shuffled:", shuffled);
     } //display your optionns i html
+
+    // show whichones clicked and right
+    
+    answersShow.forEach((btn) => { //function for changing the bg color on right/ wrong answer
+      btn.addEventListener("click", () => {
+        if (btn.innerText === data.nicolle[currentIndex].answer){
+          btn.style.backgroundColor = "#72bf6a"
+        } else{
+          btn.style.backgroundColor = "#f94449";
+        }
+      });
+    });
+   function resetBgColor(){ // function clearing bg color on answer when clicked to next question
+    answersShow.forEach((btn) => {
+      btn.style.backgroundColor = "#ffb100";
+    })
+   }
+
+   powerUp.addEventListener("click", )
+
   });
 
 // js for switching betwheen pages
@@ -65,17 +88,17 @@ let closeQuiz = document.getElementById("closeQuiz");
 
 let quiz = document.getElementById("quiz");
 let startScreen = document.getElementById("startScreen");
-
-startTheQuiz.addEventListener("click", () => {
-  startScreen.hidden = true;
-  quiz.hidden = false;
-});
-closeQuiz.addEventListener("click", () => {
-  startScreen.hidden = false;
-  quiz.hidden = true;
-});
-backToHomePage.addEventListener("click", () => {
-  startScreen.hidden = true;
-  // HERE COMES THE HOME PAGE.hidden = true;
-});
-
+startScreen.hidden = true;
+quiz.hidden = false;
+// startTheQuiz.addEventListener("click", () => {
+//   startScreen.hidden = true;
+//   quiz.hidden = false;
+// });
+// closeQuiz.addEventListener("click", () => {
+//   startScreen.hidden = false;
+//   quiz.hidden = true;
+// });
+// backToHomePage.addEventListener("click", () => {
+//   startScreen.hidden = true;
+//   // HERE COMES THE HOME PAGE.hidden = true;
+// });
