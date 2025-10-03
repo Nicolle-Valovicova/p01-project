599b4121e3c21c7680fabdec0bf0351016f0e0f2
Nicolle Valovicova <302245247@student.rocmondriaan.nl>
2025-10-03 09:35:56 +0200

    alles responsief en feeback scherm weergeeft

--- script-nicolle.js
+++ script-nicolle.js
@@ -3,7 +3,13 @@ let answersShow = document.querySelectorAll(".answersShow");
 let prevBtn = document.getElementById("backbtn");
 let nextBtn = document.getElementById("nextBtn");
 let powerUp = document.getElementById("powerUp");
-
+let scoreShow = document.getElementById("score");
+let winFeedbackScreen = document.getElementById("win");
+let loseFeedbackScreen = document.getElementById("lose");
+let finalScore = document.querySelector(".finalScore");
+let retryBtn = document.querySelector(".rettryBtn");
+let homeBtn = document.querySelector(".homeBtn");
+let score = 0;
 let ourQuestions = "questions.json";
 // shuffle function for ramdom ex. answers
 const shuffle = (array) => {
@@ -19,7 +25,7 @@ const shuffle = (array) => {
 fetch(ourQuestions)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.nicolle);
+    // //console.log(data.nicolle);
     let currentIndex = 0;
     //shuffled options for quiz
 
@@ -31,11 +37,12 @@ fetch(ourQuestions)
         showQuestion(currentIndex);
         showOptions(currentIndex);
         resetBgColor();
+        allVisible();
       } else {
-        console.log("and nothing shows");
+        endQuiz();
       }
-      console.log("btn clicked");
     });
+
     prevBtn.addEventListener("click", function () {
       if (currentIndex > 0) {
         currentIndex--;
@@ -43,6 +50,7 @@ fetch(ourQuestions)
         showQuestion(currentIndex);
         showOptions(currentIndex);
         resetBgColor();
+        allVisible();
       } else {
         console.log("nothing works");
       }
@@ -53,32 +61,75 @@ fetch(ourQuestions)
     function showOptions(currentIndex) {
       const opts = data.nicolle[currentIndex].options;
       const shuffled = shuffle([...opts]);
-      answersShow.forEach((li, i) => { // show the options fron json array in the html shuffled
+      answersShow.forEach((li, i) => {
+        // show the options fron json array in the html shuffled
         li.innerHTML = shuffled[i];
       });
-      console.log("options:", opts);
-      console.log("shuffled:", shuffled);
-    } //display your optionns i html
+      // //console.log("options:", opts);
+      //// console.log("shuffled:", shuffled);
+    } //display your optionns in html
 
     // show whichones clicked and right
-    
-    answersShow.forEach((btn) => { //function for changing the bg color on right/ wrong answer
+    //function for changing the bg color on right/ wrong answer
+    answersShow.forEach((btn) => {
       btn.addEventListener("click", () => {
-        if (btn.innerText === data.nicolle[currentIndex].answer){
-          btn.style.backgroundColor = "#72bf6a"
-        } else{
+        if (btn.innerText === data.nicolle[currentIndex].answer) {
+          btn.style.backgroundColor = "#72bf6a";
+          drawScore();
+        } else {
           btn.style.backgroundColor = "#f94449";
         }
       });
     });
-   function resetBgColor(){ // function clearing bg color on answer when clicked to next question
-    answersShow.forEach((btn) => {
-      btn.style.backgroundColor = "#ffb100";
-    })
-   }
+    // function clearing bg color on answer when clicked to next question
+    function resetBgColor() {
+      answersShow.forEach((btn) => {
+        btn.style.backgroundColor = "#ffb100";
+      });
+    }
 
-   powerUp.addEventListener("click", )
+    powerUp.addEventListener("click", function () {
+      const correctAns = data.nicolle[currentIndex].answer;
+      const buttons = Array.from(answersShow);
+      const correcBtn = buttons.find((b) => b.innerText === correctAns);
+      const wrongBtn = buttons.filter((b) => b.innerText !== correctAns);
 
+      const keepWrong = wrongBtn[Math.floor(Math.random() * wrongBtn.length)];
+
+      wrongBtn.forEach((b) => {
+        if (b !== keepWrong) b.style.visibility = "hidden";
+      });
+    });
+    function allVisible() {
+      answersShow.forEach((b) => {
+        b.style.visibility = "visible";
+      });
+    }
+
+    // draw score functiion
+    function drawScore() {
+      score++;
+      scoreShow.innerText = "Score: " + score + "/20";
+      finalScore.innerText = "Final score: " + score + "/20";
+      console.log(finalScore.innerText);
+    }
+    function endQuiz() {
+      quiz.hidden = true;
+      toggleFeedback();
+      console.log("quiz finished score: ", score);
+    }
+    // code for showing feedbackscreen
+
+    function toggleFeedback() {
+      console.log("final score:", score);
+      if (score >= 10) {
+        winFeedbackScreen.style.display = "flex";
+        loseFeedbackScreen.style.display = "none";
+      } else {
+        winFeedbackScreen.style.display = "none";
+        loseFeedbackScreen.style.display = "flex";
+      }
+    }
   });
 
 // js for switching betwheen pages
@@ -88,17 +139,29 @@ let closeQuiz = document.getElementById("closeQuiz");
 
 let quiz = document.getElementById("quiz");
 let startScreen = document.getElementById("startScreen");
+// ! change to true when you want to hide the quiz at the start
+
 startScreen.hidden = true;
-quiz.hidden = false;
-// startTheQuiz.addEventListener("click", () => {
-//   startScreen.hidden = true;
-//   quiz.hidden = false;
-// });
-// closeQuiz.addEventListener("click", () => {
-//   startScreen.hidden = false;
-//   quiz.hidden = true;
-// });
-// backToHomePage.addEventListener("click", () => {
-//   startScreen.hidden = true;
-//   // HERE COMES THE HOME PAGE.hidden = true;
-// });
+quiz.hidden = true;
+startTheQuiz.addEventListener("click", () => {
+  startScreen.hidden = true;
+  quiz.hidden = false;
+});
+closeQuiz.addEventListener("click", () => {
+  startScreen.hidden = false;
+  quiz.hidden = true;
+});
+backToHomePage.addEventListener("click", () => {
+  startScreen.hidden = true;
+  // TODO: HERE COMES THE HOME PAGE.hidden = true;
+});
+retryBtn.addEventListener("click", () => {
+  quiz.hidden = false;
+  winFeedbackScreen.style.display = "none";
+  loseFeedbackScreen.style.display = "none";
+});
+homeBtn.addEventListener("click", () => {
+  // TODO: HERE COMES THE HOME PAGE .hidden = false;
+  winFeedbackScreen.style.display = "none";
+  loseFeedbackScreen.style.display = "none";
+});
