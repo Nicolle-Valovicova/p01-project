f28d72af1a375de056d0e6bc52e6e4dfd70722b8
Nicolle Valovicova <302245247@student.rocmondriaan.nl>
2025-10-31 17:31:33 +0100

    Deleted unnecessary folder

--- deze map/script.js
+++
@@ -1,8 +0,0 @@
-const geboortejaarInput = prompt("Wat is je geboortejaar?");
-const geboortejaar = parseInt(geboortejaarInput);
-console.log("geboortejaar is-", geboortejaar);
-
-const huldingjaar = 3025;
-const leeftijd = huldingjaar - geboortejaar;
-console.log(" de bereikende leeftijd is -", leeftijd);
-
